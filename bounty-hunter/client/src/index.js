90b0193843c230397a04eead0a10da0dde97ad67
McKay Burnett <mckay.burnett@gmail.com>
2023-01-07 22:33:17 -0700

    added starwars font and syled sabers and skull

--- bounty-hunter/client/src/index.js
+++ bounty-hunter/client/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link href="https://fonts.cdnfonts.com/css/star-wars" rel="stylesheet"></link>
     <App />
   </React.StrictMode>
 );
