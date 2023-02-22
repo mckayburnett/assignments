21e88815fd138718b99f02b3a3312c0e0875623d
McKay Burnett <mckay.burnett@gmail.com>
2023-02-21 21:55:06 -0700

    updating home page

--- profit-dynamics/client/src/App.js
+++ profit-dynamics/client/src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Sales from "./components/Sales.js"
+import Home from "./components/Home.js"
 
 export default function App(){
 
   const [sales, setSales] = useState([])
-  
 
   function getSales(){
     axios.get("/sales")
@@ -14,6 +14,30 @@ export default function App(){
       .catch(err => console.log(err.response.data.errMsg))
     console.log("working")
     console.log(sales[0])
+  }
+
+  function addSale(newSale){
+    axios.post("/sales", newSale)
+      .then(res => {
+        setSales(prevSales => [...prevSales, res.data])
+      })
+      .catch(err => console.log(err))
+  }
+
+  function deleteSale(saleId){
+    axios.delete(`/sales/${saleId}`)
+      .then(res => {
+        setSales(prevSales => prevSales.filter(sale => sale._id !== saleId))
+      })
+  }
+
+  function editSale(updates, saleId){
+    axios.put(`/sales/${saleId}`, updates)
+      .then(res => {
+        setSales(prevSales => prevSales.map(sale => sale._id !== saleId ? sale : res.data))
+      })
+      .catch(err => console.log(err))
+    window.location.reload()
   }
   useEffect(() => {
     getSales()
@@ -30,13 +54,13 @@ export default function App(){
         </Link>
       </nav>
       <div className="signIn">
-        <Link to="/sales" className="salesLink">
+        <Link to="/sales" className="signInLink">
           Sign In
         </Link>
       </div>
       <Routes>
-        {/* <Route path="/" element={<Home />}/>
-        <Route path="/sales" element={<Sales />}/> */}
+        <Route path="/" element={<Home />}/>
+        <Route path="/sales" element={<Sales />}/>
       </Routes>
     </div>
   )
---
+++ profit-dynamics/client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react"
+
+export default function Home(){
+
+    return (
+        <div className="homeWrapper">
+            <img src="https://wallpaperaccess.com/full/732220.jpg" alt="" className="homeBackground"/>
+        </div>
+    )
+}
