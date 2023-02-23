5f0e5614652614ce086153540c9d9eaf724ba5d8
McKay Burnett <mckay.burnett@gmail.com>
2023-02-22 21:21:09 -0700

    created sign in with email and password

--- profit-dynamics/client/src/App.js
+++ profit-dynamics/client/src/App.js
@@ -7,15 +7,20 @@ import Home from "./components/Home.js"
 export default function App(){
 
   const [sales, setSales] = useState([])
+  const [signInToggle, setSignInToggle] = useState(false)
+  const [confirmToggle, setConfirmToggle] = useState()
+  const [incorrectToggle, setIncorrectToggle] = useState(false)
+  const [emails, setEmails] = useState([])
+  const [passwords, setPasswords] = useState([])
 
+//axios functions
   function getSales(){
     axios.get("/sales")
       .then(res => setSales(res.data))
       .catch(err => console.log(err.response.data.errMsg))
     console.log("working")
-    console.log(sales[0])
+    console.log(sales)
   }
-
   function addSale(newSale){
     axios.post("/sales", newSale)
       .then(res => {
@@ -23,14 +28,12 @@ export default function App(){
       })
       .catch(err => console.log(err))
   }
-
   function deleteSale(saleId){
     axios.delete(`/sales/${saleId}`)
       .then(res => {
         setSales(prevSales => prevSales.filter(sale => sale._id !== saleId))
       })
   }
-
   function editSale(updates, saleId){
     axios.put(`/sales/${saleId}`, updates)
       .then(res => {
@@ -43,19 +46,114 @@ export default function App(){
     getSales()
   },[])
   
+//other functions
+  function handleChangeEmails(e){
+    setEmails(e.target.value)
+    console.log("emails: ", emails)
+  }
+  function handleChangePasswords(e){
+    setPasswords(e.target.value)
+    console.log("passwords: ", passwords)
+  }
+  function confirming(){
+    if((emails === "mckay.burnett@gmail.com" && passwords === "macdaddy58") || (emails === "dyllan.m.phillips@getMaxListeners.com" && passwords === "bigd58")){
+      setConfirmToggle(true)
+    } else {
+      setIncorrectToggle(true)
+    }
+  }
+  function signIn(){
+    setSignInToggle(false)
+    setConfirmToggle(false)
+    setEmails("")
+    setPasswords("")
+    incorrectToggle(false)
+  }
+  function signOut(){
+    setSignInToggle(false)
+    setConfirmToggle(false)
+    setEmails("")
+    setPasswords("")
+    incorrectToggle(false)
+  }
+  function home(){
+    setSignInToggle(false)
+    setConfirmToggle(false)
+    setEmails("")
+    setPasswords("")
+    incorrectToggle(false)
+  }
+  
+console.log(emails)
+
   return (
     <div className="appWrapper">
       <nav className="navbar">
-        <Link to="/" className="homeLink">
+        <Link 
+          to="/" 
+          className="homeLink"
+          onClick={home}
+        >
           Home
         </Link>
-        <Link to="/" className="signOutLink">
+        <Link 
+          to="/" 
+          className="signOutLink"
+          onClick={signOut}
+        >
           Sign Out
         </Link>
-        <Link to="/sales" className="signInLink">
+        <button 
+          className="signInButton"
+          onClick={() => setSignInToggle(true)}
+        >
           Sign In
-        </Link>
+        </button>
       </nav>
+      { signInToggle ?
+        <div className="signInForm">
+          <input 
+            className="email"
+            placeholder="Email Address"
+            type="text"
+            name="email"
+            onChange={handleChangeEmails}
+          />
+          <input 
+            className="password"
+            placeholder="Password"
+            type="password"
+            name="password"
+            onChange={handleChangePasswords}
+          />
+          { confirmToggle ?
+          <div>
+            <p className="credentials">Credentials Confirmed!</p>
+            <Link 
+            to="/sales" 
+            className="signInLink"
+            onClick={signIn}
+            >
+              Continue
+            </Link>
+          </div>
+          :
+          <button 
+            className="confirmButton"
+            onClick={confirming}
+          >
+            {incorrectToggle ?
+            <p className="credentials2">Email or Password incorrect please try again</p>
+            :
+            <></>
+          }
+            Confirm
+          </button>
+          }
+        </div>
+      :
+      <></>
+      }
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/sales" element={<Sales />}/>
--- profit-dynamics/client/src/components/Home.js
+++ profit-dynamics/client/src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home(){
             <link href='https://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'></link>
             <img src="https://wallpaperaccess.com/full/732220.jpg" alt="" className="homeBackground"/>
             <div className="companyWrapper">
-                <div className="companyName" data-text="Applicable Development">Applicable Development</div>
+                <div className="companyName" data-text="Dynamic Development">Dynamic Development</div>
             </div>
         </div>
     )
