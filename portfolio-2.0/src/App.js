100b97f73196876a9679313c781e2c0c1e9e48b2
McKay Burnett <mckay.burnett@gmail.com>
2023-04-10 22:18:08 -0600

    working on intro animation

--- portfolio-2.0/src/App.js
+++ portfolio-2.0/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import pic00 from "./images/pic00.png"
 
 export default function App (){
 
@@ -7,13 +7,18 @@ export default function App (){
     <div className="appWrap">        
       <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
       <nav className="nav">
-        <h1>Top</h1>
-        <h1>Proficiency</h1>
-        <h1>Portfolio</h1>
-        <h1>Contact</h1>
+        <h1 className="logo">McKay Burnett</h1>
+        <div className="nav-links">
+          <h1>Top</h1>
+          <h1>Proficiency</h1>
+          <h1>Portfolio</h1>
+          <h1>Contact</h1>
+        </div>
       </nav>
       <body>
-        <h1 className="test anim-typewriter">{ `{ Hello. I'm `}<strong>McKay Burnett</strong>{`, a Full-Stack Developer, and this is my PORTFOLIO. }`}</h1>
+        <img className="me" src={pic00} alt="" />
+        <h1 className="intro anim-typewriter">{ `Hello. I'm `}<strong>MCKAY BURNETT,</strong></h1>
+        <h1 className="intro-2 anim-typewriter">{` a Full-Stack Developer, and this is my PORTFOLIO." `}</h1>
       </body>
     </div>
   )
