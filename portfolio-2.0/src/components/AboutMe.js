0a38c8c18d167bb3c2ed5c5d6a7fa5e191ac2e09
McKay Burnett <mckay.burnett@gmail.com>
2023-04-15 14:40:09 -0600

    scrolling

---
+++ portfolio-2.0/src/components/AboutMe.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function AboutMe (){
+
+    return (
+    <div className="aboutMeWrapper">
+        <h1 className="test">hello world</h1>
+    </div>
+    )
+}
---
+++ portfolio-2.0/src/components/Home.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react"
+import Typewriter from "typewriter-effect"
+import pic00 from "../images/pic00.png"
+import typing from "../typing.mp3"
+import { FaArrowRight } from 'react-icons/fa'
+
+export default function Home(){
+
+  //setTimeout(() => typing.play(), 500)
+  let audio = new Audio(typing)
+  
+  //enter button
+  const [enter, setEnter] = useState(false)
+  const [showButton, setShowButton] = useState(false)
+  function handleEnter(){
+    setEnter(true);
+    setTimeout(() => {
+      setShowButton(true);
+    }, 7300)
+    setTimeout(() => {
+      audio.play()
+      setTimeout(() => {
+        audio.pause()
+      }, 1100)
+    }, 300);
+    setTimeout(() => {
+      audio.play()
+      setTimeout(() => {
+        audio.pause()
+        setTimeout(() => {
+          audio.play()
+          setTimeout(() => {
+            audio.pause()
+            setTimeout(() => {
+              audio.play()
+              setTimeout(() => {
+                audio.pause()
+              }, 1200)
+            }, 1700)
+          }, 2900)
+        }, 1300)
+      }, 2200)
+    }, 2700);
+  }
+
+  //about me button
+  function clickAboutMe(){
+    window.scrollTo({
+      left: auto,
+      behavior: "smooth",
+    });
+  }
+  
+  return (
+    <div className="appWrap">        
+      <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css?family=Inspiration&display=swap" rel="stylesheet"></link>
+      <nav className="nav">
+        <h1 className="logo">McKay Burnett</h1>
+        <div className="nav-links">
+          <h1>Top</h1>
+          <h1>About</h1>
+          <h1>Proficiency</h1>
+          <h1>Portfolio</h1>
+          <h1>Contact</h1>
+        </div>
+      </nav>
+    {enter ? 
+      <body>
+        <img className="me" src={pic00} alt="" />
+        <div className="typewriter">
+          <Typewriter
+
+            onInit={(typewriter) => {
+
+              typewriter
+                .changeDelay(50)
+                .typeString(`{ Hello! /}`)
+                .pauseFor(1500)
+                .deleteAll(20)
+
+                .changeDelay(50)
+                .typeString("My name is <strong>MCKAY BURNETT</strong>")
+                .pauseFor(1500)
+                .deleteAll(10)
+
+                .changeDelay(50)
+                .typeString("I'm a <strong>FULL-STACK DEVELOPER</strong> and...<br></br>")
+                .pauseFor(1500)
+
+                .changeDelay(50)
+                .typeString("This is my <strong>PORTFOLIO</strong>")
+                .start();
+
+            }}
+          />
+        </div>
+        <button className={showButton ? `button-aboutMe` : `hidden`} onClick={clickAboutMe}>About Me <FaArrowRight className="arrow"/></button>
+      </body>
+      :
+      <div className="enterContainer">
+        <button className="enter" onClick={handleEnter}>Enter</button>
+        <img className="me-2" src={pic00} alt="" />
+      </div>
+      }
+    </div>
+  )
+}
