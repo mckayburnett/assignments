ab0e93610e84a38659d7a824d4e01bf3b92cddd4
McKay Burnett <mckay.burnett@gmail.com>
2023-04-15 21:38:19 -0600

    added explosion

--- portfolio-2.0/src/components/AboutMe.js
+++ portfolio-2.0/src/components/AboutMe.js
@@ -1,11 +1,15 @@
 import React from "react"
 import pic01 from "../images/pic01.png"
+import { GiExplosiveMaterials } from 'react-icons/gi'
 
 export default function AboutMe (){
 
     return (
     <div className="aboutMeWrapper">
         <img className="aboutMe-pic" src={pic01} alt="" />
+        <div className="explosionContainer">
+            < GiExplosiveMaterials className="explosion" />
+        </div>
     </div>
     )
 }
