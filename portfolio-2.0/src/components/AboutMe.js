52d6ef20093c3a9a0412511a42f04d38583a55e3
McKay Burnett <mckay.burnett@gmail.com>
2023-04-24 14:40:18 -0600

    laptop pic

--- portfolio-2.0/src/components/AboutMe.js
+++ portfolio-2.0/src/components/AboutMe.js
@@ -11,11 +11,13 @@ export default function AboutMe (){
     return (
     <div className="aboutMeWrapper">
         { meButton ? 
+        <>
+        <img className="aboutMe-background" src={pic02} alt="" />
         <div className="compFull">
-            <img className="aboutMe-background" src={pic02} alt="" />
             <img className="aboutMe-pic" src={pic01} alt="" />
             <h2 className="aboutMe-description"><strong className="aboutMe-strong">A developer out of Riverton, UT.</strong><br></br>To me, Computer Programming isn't just a career, it's a hobby.<br></br>I love creating clean, effective, and useful code that makes life easier!</h2>
         </div>
+        </>
         :
         <div className="compEmpty">
             <img className="aboutMe-background" src={pic02} alt="" />
