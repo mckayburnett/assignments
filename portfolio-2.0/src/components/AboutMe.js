07ebea407e4c7a31fd7102b5dae0a38536c3b46b
McKay Burnett <mckay.burnett@gmail.com>
2023-04-24 22:37:53 -0600

    laptop editing

--- portfolio-2.0/src/components/AboutMe.js
+++ portfolio-2.0/src/components/AboutMe.js
@@ -2,13 +2,14 @@ import React, { useState } from "react"
 import pic01 from "../images/pic01.png"
 import pic02 from "../images/pic02.png"
 import pic03 from "../images/pic03.png"
-import cursor from "../images/cursor.png"
+import coding from "../coding.mp3"
 
 
 export default function AboutMe (){
 
     //laptop button
     const [meButton, setMeButton] = useState(false)
+    const [delayedButton, setDelayedButton] = useState(false)
 
     //coding string
     const str = `<div className="aboutMeWrapper">
@@ -37,15 +38,22 @@ export default function AboutMe (){
     }
 </div>`
 
-    //cursor
-    const cursorStyle = `cursor: url(${cursor}), auto;
- `
+    //coding button
+    let audio = new Audio(coding)
+    function handleCoding(){
+        setTimeout(() => {
+            setMeButton(true);
+            clearTimeout()
+            setDelayedButton(true);
+        }, 500)
+        audio.play()
+    }
     return (
     <div className="aboutMeWrapper" >
         { meButton ? 
         <>
             <img className="aboutMe-laptop" src={pic02} alt="" />
-            <div className="compFull">
+            <div className={delayedButton ? "compFull" : "hidden"}>
                 <img className="aboutMe-pic" src={pic01} alt="" />
                 <h2 className="aboutMe-description"><h1 className="description-one">Front and Back-End developer</h1><br></br>I'm a Utah based Front and Back-End developer. To me, Computer Programming isn't just a career, it's a hobby. I love creating clean, effective, and useful code that makes life easier!</h2>
                 <strong className="aboutMe-strong">McKay Burnett</strong>
@@ -59,7 +67,7 @@ export default function AboutMe (){
                 <h3 className="code" style = {{ whiteSpace: 'pre-wrap'}}>
                    {str}
                 </h3>
-                <button className="codeButton" onClick={() => setMeButton(true)}>Click to run code!</button>
+                <button className="codeButton" onClick={handleCoding}>Click to run code!</button>
             </div>
         </>
         }
