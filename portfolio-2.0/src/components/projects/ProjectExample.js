287a8842359935909b5c5ed5163147348319d972
McKay Burnett <mckay.burnett@gmail.com>
2023-04-29 23:03:16 -0600

    project example design

--- portfolio-2.0/src/components/projects/ProjectExample.js
+++ portfolio-2.0/src/components/projects/ProjectExample.js
@@ -10,20 +10,69 @@ import travelJournal from "../../images/travelJournal.png"
 import cssZenGarden from "../../images/cssZenGarden.png"
 
 
-export default function ProjectExample(){
+export default [
+    {
+        id: 0,
+        title: "Dynamic Development LLC",
+        image: {dynamicDevelopmentLLC},
+        description: ""
+    },
+    {
+        id: 1,
+        title: "Jeopardy",
+        image: {jeopardy},
+        description: ""
+    },
+    {
+        id: 2, 
+        title: "Bounty Hunter",
+        image: {bountyHunter},
+        description: ""
+    },
+    {
+        id: 3,
+        title: "Meme Generator",
+        image: {memeGenerator},
+        description: ""
+    },
+    {
+        id: 4,
+        title: "Simulation  RPG",
+        image: {simulationRPG},
+        description: ""
+    },
+    {
+        id: 5,
+        title: "Gradient Code Generator",
+        image: {gradientCodeGenerator},
+        description: ""
+    },
+    {
+        id: 6,
+        title: "Vacation Spots",
+        image: {vacationSpots},
+        description: ""
+    },
+    {
+        id: 7,
+        title: "Travel Journal",
+        image: {travelJournal},
+        description: ""
+    },
+    {
+        id: 8,
+        title: "CSS Zen Garden",
+        image: {cssZenGarden},
+        description: ""
+    }
+]
+        // <img src={dynamicDevelopmentLLC} alt="" className="dDLLC"></img>
+        // <img src={jeopardy} alt="" className="jeopardy"></img>
+        // <img src={bountyHunter} alt="" className="bountyHunter"></img>
+        // <img src={memeGenerator} alt="" className="memeGenerator"></img>
+        // <img src={simulationRPG} alt="" className="simulationRPG"></img>
+        // <img src={gradientCodeGenerator} alt="" className="gradientCodeGenerator"></img>
+        // <img src={vacationSpots} alt="" className="vacationSpots"></img>
+        // <img src={travelJournal} alt="" className="travelJournal"></img>
+        // <img src={cssZenGarden} alt="" className="cssZenGarden"></img>
 
-
-    return(
-        <>
-        <img src={dynamicDevelopmentLLC} alt="" className="dDLLC"></img>
-        <img src={jeopardy} alt="" className="jeopardy"></img>
-        <img src={bountyHunter} alt="" className="bountyHunter"></img>
-        <img src={memeGenerator} alt="" className="memeGenerator"></img>
-        <img src={simulationRPG} alt="" className="simulationRPG"></img>
-        <img src={gradientCodeGenerator} alt="" className="gradientCodeGenerator"></img>
-        <img src={vacationSpots} alt="" className="vacationSpots"></img>
-        <img src={travelJournal} alt="" className="travelJournal"></img>
-        <img src={cssZenGarden} alt="" className="cssZenGarden"></img>
-        </>
-    )
-}
