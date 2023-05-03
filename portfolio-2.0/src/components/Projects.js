8b20ee7361495535ee65b77e4198482e963c2c3e
McKay Burnett <mckay.burnett@gmail.com>
2023-05-02 19:27:35 -0600

    project template work

--- portfolio-2.0/src/components/Projects.js
+++ portfolio-2.0/src/components/Projects.js
@@ -3,17 +3,39 @@ import projectData from "../projectData.js"
 
 const Projects = React.forwardRef((props, ref) => {
 
+    const templates = []
+    const titles = [
+        "Dynamic Development",
+        "Jeopardy",
+        "Bounty Hunter",
+        "Meme Generator",
+        "Simulation RPG",
+        "Gradient Code Generator",
+        "Vacation Spots",
+        "Travel Journal",
+        "CSS Zen Garden",
+    ]
+    let num = 0
+
     const projectTemplate = projectData.map(project => {
-        console.log(project)
+        console.log("project",project)
+        templates.push(project)
+        console.log("templates", templates[0])
         return(
             <div className="templateWrapper">
-                <img className="templateImage"src={project.image} alt="" />
+                <div className="template">
+                    <img className="templateImage"src={project.image} alt="" />
+                </div>
             </div>
         )
     })
     
     return (
         <div className="projectsWrapper" id="projects" ref={ref}>
+            <div className="titlesWrapper">
+                <h1 className="titles">{titles[num]}</h1>
+                
+            </div>
             {projectTemplate}
         </div>
     )
