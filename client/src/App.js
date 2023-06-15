54ac923a144c8b9be15b8c3b1fff3eebb03a9dcf
McKay Burnett <mckay.burnett@gmail.com>
2023-06-15 16:15:12 -0600

    check and remove passwords and protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/Navbar.js'
 import Auth from './components/Auth.js'
 import Profile from './components/Profile.js'
 import Public from './components/Public.js'
+import ProtectedRoute from "./components/ProtectedRoute.js"
 import { UserContext } from './context/UserProvider.js'
 
 export default function App(){
   const { token, logout } = useContext(UserContext)
   return (
     <div className="app">
-      <Navbar logout={logout}/>
+      { token && <Navbar logout={logout}/> }
       <Routes>
         <Route 
           path="/" 
@@ -18,11 +19,15 @@ export default function App(){
         />
         <Route 
           path="/profile"
-          element={<Profile />}
+          element={<ProtectedRoute token={token} redirectTo="/">
+            <Profile />
+          </ProtectedRoute>}
         />
         <Route 
           path="/public"
-          element={<Public />}
+          element={<ProtectedRoute token={token} redirectTo="/">
+          <Public />
+        </ProtectedRoute>}
         />
       </Routes>
     </div>
