f099bc3a092b0400c39ee531f4e53fa99bb28360
McKay Burnett <mckay.burnett@gmail.com>
2023-07-14 15:57:01 -0600

    reply text

--- rtv/routes/replyRouter.js
+++ rtv/routes/replyRouter.js
@@ -5,31 +5,54 @@ const User = require('../models/user')
 const Reply = require('../models/reply')
 
 //add reply
+// replyRouter.put('/:issueId', (req, res, next) => {
+//     Issue.findOne( {_id: req.params.issueId}, (err, foundIssue) => {
+//         if(err){
+//             res.status(500)
+//             return next(err)
+//         }
+//         const reply = foundIssue.Reply
+//         reply.text.push(req.body)
+//         foundIssue.save((err, updatedIssue) => {
+//             if(err){
+//                 res.status(500)
+//             }
+//             return res.status(201).send(updatedIssue)
+//         })
+//     })
+// })
 replyRouter.put('/:issueId', (req, res, next) => {
-    Issue.findOne( {_id: req.params.issueId}, (err, foundIssue) => {
-        if(err){
-            res.status(500)
-            return next(err)
+    Issue.findOne({_id: req.params.issueId})
+      .populate('reply') // Populate the Reply field
+      .exec((err, foundIssue) => {
+        if (err) {
+          res.status(500);
+          return next(err);
         }
-        const text = req.body
-        const newReply = new Reply({
-            text
-        })
-        foundIssue.updateOne(
-            {reply : newReply},
-            {new: true},
-            (err, updatedReply) => {
-                if(err){
-                    res.status(500)
-                    return next(err)
-                }
-                return res.status(201).send(updatedReply)
-            }
-        )
-    })
-    // Issue.findOneAndUpdate(
-    //     {_id: req.params.issueId},
-    //     {$push: {reply: req.body.text}},
+        if (!foundIssue) {
+          res.status(404);
+          return next(err);
+        }
+        if (!foundIssue.reply) {
+            foundIssue.reply = {
+              text: [] // Initialize the text array if it's null
+            };
+          }
+        const reply = foundIssue.reply;
+        reply.text.push({ text: req.body.text });
+        foundIssue.save((err, updatedIssue) => {
+          if (err) {
+            res.status(500);
+            return next(err);
+          }
+          return res.status(201).send(updatedIssue);
+        });
+      });
+  });
+  
+    // Reply.findOneAndUpdate(
+    //     {Issue: {reply: req.params.issueId}},
+    //     {$push: {text: req.body.text}},
     //     {new: true},
     //     (err, updatedReply) => {
     //         if(err){
@@ -39,8 +62,6 @@ replyRouter.put('/:issueId', (req, res, next) => {
     //         return res.status(201).send(updatedReply)
     //     }
     // )
-})
-
 
 
 //get all replies
