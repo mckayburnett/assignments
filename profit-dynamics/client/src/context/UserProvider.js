b5ebd08cbdad023017ab4f3a5dad44364b3fed8a
McKay Burnett <mckay.burnett@gmail.com>
2023-08-07 15:47:42 -0600

    starting profit-dynamics as capstone

---
+++ profit-dynamics/client/src/context/UserProvider.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+import React, { useState } from 'react'
+
+export default function UserProvider(props){
+
+    const initState = {
+        user: JSON.parse(localStorage.getItem("user")) || {},
+        token: localStorage.getItem("token") || ""
+    }
+    const [userState, setUserState] = useState(initState)
+
+    function signup(credentials){
+        axios.post("/auth/signup", credentials)
+            .then(res => {
+                const { user, token } = res.data
+                localStorage.setItem("token", token)
+                localStorage.setItem("user", JSON.stringify(user))
+            })
+    }
+
+}
--- profit-dynamics/server.js
+++ profit-dynamics/server.js
@@ -1,17 +1,24 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
+const { expressjwt } = require("express-jwt")
 
 app.use(express.json())
 app.use(morgan("dev"))
 
 mongoose.connect("mongodb://localhost:27017/fake-data-db", () => console.log("connected to fake-data-db"))
 
-app.use("/sales", require("./routes/salesRouter.js"))
+app.use("/auth", require("./routes/authRouter.js"))
+app.use("/api", expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
+app.use("/api/sales", require("./routes/salesRouter.js"))
 
 app.use((err, req, res, next) => {
     console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
     return res.send({errMsg: err.message})
 })
 
