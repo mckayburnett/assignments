cd111a14940f2d292a7ebc23504daaf86196ad86
McKay Burnett <mckay.burnett@gmail.com>
2023-08-10 17:13:29 -0600

    styling

--- profit-dynamics/client/src/components/Navbar.js
+++ profit-dynamics/client/src/components/Navbar.js
@@ -7,15 +7,10 @@ export default function Navbar(props){
     const { logout } = props
 
   return (
-    <>
     <div className="navbar">
       <Link to="/" className="navHome">Home</Link>
       <Link to="/sales" className="navSales">Sales</Link>
       <h1 className="navLog" onClick={ logout }>Logout</h1>
-    </div>
-    <div className="spacer">
-      &nbsp;
-    </div>
-    </>
+    </div> 
   )
 }
