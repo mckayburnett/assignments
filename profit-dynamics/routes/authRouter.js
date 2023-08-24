cb732a904fc9fb372915247036a1422ea119c98e
McKay Burnett <mckay.burnett@gmail.com>
2023-08-24 15:01:43 -0600

    working on toy problems

--- profit-dynamics/routes/authRouter.js
+++ profit-dynamics/routes/authRouter.js
@@ -20,7 +20,7 @@ authRouter.post("/signup", (req, res, next) => {
                 return next(err)
             }
             const token = jwt.sign(savedUser.withoutPassword(), process.env.SECRET)
-            return res.status(201).send({ token, user: savedUser.withoutPassord() })
+            return res.status(201).send({ token, user: savedUser.withoutPassword() })
         })
     })
 })
@@ -33,22 +33,20 @@ authRouter.post("/login", (req, res, next) => {
         }
         if(!user){
             res.status(403)
-            return next(new Error("Email or Password incorrect"))
+            return next(new Error("Email and/or Password incorrect"))
         }
         user.checkPassword(req.body.password, (err, isMatch) => {
             if(err){
                 res.status(403)
-                return next(new Error("Email or Password incorrect"))
+                return next(new Error("Email and/or Password incorrect"))
             }
             if(!isMatch){
                 res.status(403)
-                return next(new Error("Username or Password incorrect"))
+                return next(new Error("Email and/or Password incorrect"))
             }
             const token = jwt.sign(user.withoutPassword(), process.env.SECRET)
-            return res.status(200).send({ token, user })
+            return res.status(200).send({ token, user: user.withoutPassword() })
         })
-        const token = jwt.sign(user.toObject(), process.env.SECRET)
-        return res.status(200).send({ token, user: user.withoutPassword() })
     })
 })
 
